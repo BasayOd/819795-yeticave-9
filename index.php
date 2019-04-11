1470ff952543a2e90f215b151a9bde877476fab9
Basay <Basay1980@gmail.com>
2019-04-11 15:17:11 +0300

    gh

--- index.php
+++ index.php
@@ -27,25 +27,6 @@ $user_name = 'Vasil Litvinenko'; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-<<<<<<< HEAD
-           <?php if($is_auth==1){?>
-            <div class="user-menu__logged">
-            <p>'Vasyl Litvinenko'</p>
-            <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-            <a class="user-menu__logout" href="#">Выход</a>
-            </div>
-           <?php }else{?>
-            <ul class="user-menu__list">
-              <li class="user-menu__item">
-                 <a href="#">Регистрация</a>
-              </li>
-              <li class="user-menu__item">
-                 <a href="#">Вход</a>
-              </li>
-  </ul>
-           <?php }?>
-        <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-=======
 
         <?php
           if ($is_auth==1){ ?>
@@ -65,7 +46,6 @@ $user_name = 'Vasil Litvinenko'; // укажите здесь ваше имя
           </ul>
           <?php }
         ?>
->>>>>>> 3e76108c43716a08f089368e13a29fe13e10ee36
 
         </nav>
     </div>
