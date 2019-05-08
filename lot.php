f6df7867236e6ab8a1b6f87ad4c14e3fc9c2e4dd
Basay <Basay1980@gmail.com>
2019-05-08 12:06:36 +0300

    работа над ошибками

--- lot.php
+++ lot.php
@@ -3,18 +3,18 @@ require_once ('functions.php');
 require_once ('init.php');
 require_once ('helpers.php');
 date_default_timezone_set('Europe/Kiev');
-$id_number=$_GET['id']??'1';
+$id_number=$_GET['id']??'9';
 $lots=get_data_all($link,'SELECT l.*, c.name AS category FROM lots l 
                               LEFT JOIN categories c ON l.category_id=c.id 
                               WHERE l.id='.$id_number.';');
 $cats=get_data_all($link,'SELECT name, icon FROM categories');
 $bets=get_data_all($link, 'SELECT b.*, u.name AS user_name FROM bets b 
                                LEFT JOIN users u ON b.user_id=u.id 
-                               WHERE lot_id='.$id_number.' ORDER BY amount DESC;');
+                               WHERE b.lot_id='.$id_number.' ORDER BY amount DESC;');
 if ($lots) {
     $lot=$lots[0];
     $lot_content=include_template('lot.php',['categories'=>$cats, 'lot'=>$lot, 'bets'=>$bets]);
-    print($lot_content);
    }else{
-    print('404');};
+    $lot_content=include_template('404.php',['categories'=>$cats]);};
+print($lot_content);
 ?>
--- templates/404.php
+++ templates/404.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title><?= $lot['name'] ?></title>
+    <title>404</title>
     <link href="css/normalize.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 </head>
@@ -45,53 +45,8 @@
             </ul>
         </nav>
         <section class="lot-item container">
-            <h2><?= $lot['name'] ?></h2>
-            <div class="lot-item__content">
-                <div class="lot-item__left">
-                    <div class="lot-item__image">
-                        <img src="<?= $lot['img'] ?>" width="730" height="548" alt="Сноуборд">
-                    </div>
-                    <p class="lot-item__category">Категория: <span><?= $lot['category'] ?></span></p>
-                    <p class="lot-item__description"><?= $lot['description'] ?></p>
-                </div>
-                <div class="lot-item__right">
-                    <div class="lot-item__state">
-                        <div class="lot-item__timer timer <?= check_alarm($lot['dt_end'])?>">
-                            <?= show_remaining_time($lot['dt_end']) ?>
-                        </div>
-                        <div class="lot-item__cost-state">
-                            <div class="lot-item__rate">
-                                <span class="lot-item__amount">Текущая цена</span>
-                                <span class="lot-item__cost"><?= form_price($lot['price'])?></span>
-                            </div>
-                            <div class="lot-item__min-cost">
-                                Мин. ставка <span><?=  $lot['price']+$lot['bet_step'] ?> р</span>
-                            </div>
-                        </div>
-                        <form class="lot-item__form" action="https://echo.htmlacademy.ru" method="post" autocomplete="off">
-                            <p class="lot-item__form-item form__item form__item--invalid">
-                                <label for="cost">Ваша ставка</label>
-                                <input id="cost" type="text" name="cost" placeholder="12 000">
-                                <span class="form__error">Введите наименование лота</span>
-                            </p>
-                            <button type="submit" class="button">Сделать ставку</button>
-                        </form>
-                    </div>
-                    <div class="history">
-                        <h3>История ставок (<span><?= count($bets)?></span>)</h3>
-                        <table class="history__list">
-                            <?php foreach($bets as $key=>$value){  ?>
-                                <tr class="history__item">
-                                    <td class="history__name"><?= $value['user_name']?></td>
-                                    <td class="history__price"><?= $value['amount']?></td>
-                                    <td class="history__time"><?= $value['create_at']?></td>
-                                </tr>
-                            <?php } ?>
-
-                        </table>
-                    </div>
-                </div>
-            </div>
+            <h2>404 Страница не найдена</h2>
+            <p>Данной страницы не существует на сайте.</p>
         </section>
     </main>
 
@@ -102,7 +57,7 @@
         <ul class="nav__list container">
             <?php foreach ($categories as  $value) { ?>
                 <li class="nav__item">
-                    <a href="pages/all-lots.html"><?= strip_tags($value['name']) ?></a>
+                    <a href="pages/all-lots.html"><?= $value['name'] ?></a>
                 </li>
             <?php } ?>
         </ul>
@@ -151,3 +106,4 @@
 
 </body>
 </html>
+
